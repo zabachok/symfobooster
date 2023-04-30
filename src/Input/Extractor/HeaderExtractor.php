935da23359371227a332e2f0de05b47605547d0d
zabachok <zabachok@gmail.com>
2023-04-30 12:06:03 +0300

    Some fixes

--- src/Input/Extractor/HeaderExtractor.php
+++ src/Input/Extractor/HeaderExtractor.php
@@ -2,7 +2,12 @@
 
 namespace Zabachok\Symfobooster\Input\Extractor;
 
+use Symfony\Component\HttpFoundation\Request;
+
 class HeaderExtractor implements ExtractorInterface
 {
-
+    public function extract(Request $request, string $name): mixed
+    {
+        return $request->headers->get($name);
+    }
 }
--- src/Input/Extractor/ServerExtractor.php
+++ src/Input/Extractor/ServerExtractor.php
@@ -2,7 +2,12 @@
 
 namespace Zabachok\Symfobooster\Input\Extractor;
 
+use Symfony\Component\HttpFoundation\Request;
+
 class ServerExtractor implements ExtractorInterface
 {
-
+    public function extract(Request $request, string $name): mixed
+    {
+        return $request->server->get($name);
+    }
 }
